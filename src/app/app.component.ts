1958b1bf63b64a1265bffb6acd5a7c134e3470c6
pobras8 <pobras8@gmail.com>
2018-06-15 17:25:20 -0400

    Logger service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggerService } from './core/logger/logger.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'app';
+
+    constructor(private logger: LoggerService) {}
+
+    ngOnInit() {
+    }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { throwIfAlreadyLoaded } from './core-module-guard';
+import { LoggerService } from './logger/logger.service';
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: []
+    declarations: [],
+    providers: [
+        LoggerService
+    ]
 })
 export class CoreModule {
     constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
